062a3ffd3bf2d3b09a449f88ea884824d0522303
arman-tech <arman.projekts@gmail.com>
2022-05-01 13:06:41 -0400

    ux changes, and more localizations.

--- src/components/product-card/product-card.vue
+++ src/components/product-card/product-card.vue
@@ -10,26 +10,28 @@
         <p>{{product.description}}</p>  
 
         <div v-if="isSoldOut">
-            <div class="card-actions justify-center">
+            <div class="card-actions justify-center pt-4">
                 <blockquote class="text-2xl font-semibold italic text-center text-slate-900">
                     <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
-                        <span class="relative text-white p-2">SOLD OUT!</span>
+                        <span class="relative text-white p-2">{{ t("home.soldOut") }}</span>
                     </span>
                 </blockquote>
             </div>
         </div>
         <div v-else>
-            <div class="card-actions justify-between">
-                <div class="justify-start">
+            <div class="card-actions">
+                <div class="form-control w-full max-w-xs pt-4">
                     <select class="select select-primary" v-model="qtySelected">
-                        <option disabled selected value="0">Select quantity</option>
+                        <option disabled selected value="0">{{ t("home.selectQty") }}</option>
                         <template v-for="(e,i) in QtyRange" :key="i">
                             <option :value="minQty + i">{{ minQty + i }}</option>
                         </template>
                     </select>       
                 </div>
-                <div class="justify-end">
-                    <button class="btn btn-primary" :disabled="!(qtySelected > 0)" v-on:click="addToCart">Add to cart</button>
+            </div>
+            <div class="card-actions">
+                <div class="form-control w-full max-w-xs pt-3">
+                    <button class="btn btn-primary" :disabled="!(qtySelected > 0)" v-on:click="addToCart">{{ t("home.addToCart") }}</button>
                 </div>
             </div>
         </div>
